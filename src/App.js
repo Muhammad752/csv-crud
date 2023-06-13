93bf592157aa9749d4c2e78f705cccc878560d12
Muhammad752 <madaminov.muhammadjon@mail.ru>
2023-06-13 15:39:35 +0500

    add files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './pages/Login';
+import {BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
