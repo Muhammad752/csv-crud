cbdab3bc95834e9ab657d4492d45a6a69b29eb03
Muhammad752 <madaminov.muhammadjon@mail.ru>
2023-07-05 09:21:03 +0500

    local token

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -32,6 +32,10 @@ function classNames(...classes) {
 }
 
 export default function DataPage() {
+  localStorage.setItem(
+    "ipoteka_token",
+    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLQlpqOXEtbk9yOXdPVmhRNXFibWx0NmVJMVp0OWlOa1c3eWhVMVZSN0cwIn0.eyJleHAiOjE2ODg0Nzc0NDUsImlhdCI6MTY4ODQ3Mzg0NSwianRpIjoiYzViZWU1NGEtYzExOS00ZGMwLWJkZDItYjgyYTcxNjU4ZmEwIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL3BhcnNlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJlNmRhM2ZlNS1lODBlLTRjZTktYWEwNy03NTA3OTg4NTQ0YTkiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJwYXJzZXIiLCJzZXNzaW9uX3N0YXRlIjoiNTFjNDIxNWUtYTMzMi00Y2E2LWFhN2YtMWU2NzA3MDllZWJhIiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLXBhcnNlciIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJwYXJzZXIiOnsicm9sZXMiOlsidW1hX3Byb3RlY3Rpb24iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6IjUxYzQyMTVlLWEzMzItNGNhNi1hYTdmLTFlNjcwNzA5ZWViYSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiKzk5ODk5NjA5MzExMCJ9.eHfBphCfemPU0lMVDN15sfmR-_w2e1U5O1o9R59XRiYc8LqqtJxj1KQD7vUeYJZBGj7bjHDIXWzmK-afG8JMhkc8SwcLMjWs0rjcxY1HTwQSC_VtCit0pvNILXxVKmHizpvinShzfh3ZhhqKd6yRqU5sbiPEKrh9ewKwcSrM5fPu_LsT9nDijjThYeM2Ible2GPHD8ISMAjSYIRsO6TPM53TMD5oXJzjVtiTUiE83L-3oT7hjx_s3A5MuP90fd10PWUc6IM4ZGWhvqcg9G8YvF9C8-nuiGbgRzhe95vreZ3KDjxE5BmCzLEHbHXj0czCdvSfqdTSKarfIloWcUsg3w"
+  );
   const user = useUser();
   const [token] = useToken();
   console.log(user);
@@ -43,15 +47,20 @@ export default function DataPage() {
   useEffect(() => {
     async function loadArticle() {
       // const response = await axios.get(`/offlineData.json`);
-      const response = await axios.get(
-        process.env.REACT_APP_PROXY + `/pinfl/`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
+      try {
+        const response = await axios.get(
+          process.env.REACT_APP_PROXY + `/pinfl/`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        const newArticle = response.data;
+        if (newArticle) {
+          setData(newArticle);
         }
-      );
-      const newArticle = response.data;
-      if (newArticle) {
-        setData(newArticle);
+      } catch (e) {
+        console.log(e.message);
+        localStorage.removeItem("ipoteka_token");
       }
     }
     loadArticle();
